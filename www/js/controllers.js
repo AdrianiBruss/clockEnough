a6e15ae5ef47a14af9ea5e50a85e78af7ce7e0d9
AdrianiBruss <abrussolo@gmail.com>
2016-04-08 16:47:32 +0200

    home sign up

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -18,10 +18,31 @@ angular.module('starter.controllers', [])
 //   $scope.chat = Chats.get($stateParams.chatId);
 // })
 
-.controller('AccountCtrl', function($scope, ionicMaterialInk, ionicMaterialMotion) {
-    // Set Motion
+.controller('AccountCtrl', function($scope, ionicMaterialInk, ionicMaterialMotion, $cordovaCamera) {
+    // ionic materialize animations
     ionicMaterialMotion.fadeSlideInRight();
-
-    // Set Ink
     ionicMaterialInk.displayEffect();
+
+    $scope.capturePicture = function() {
+        var options = {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            allowEdit: true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 100,
+            targetHeight: 100,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false,
+            correctOrientation:true
+        };
+
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            var image = document.getElementById('capturedImage');
+            image.src = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+            console.error(err)
+        });
+    }
+
 });
