e9c576a543f27894bfa6b6653c0387409c110b66
AdrianiBruss <abrussolo@gmail.com>
2016-04-08 14:27:11 +0200

    controllers cleaned

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,11 +3,7 @@ angular.module('starter.controllers', [])
 .controller('EventCtrl', function($scope) {})
 
 .controller('OrganizeCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
+
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
@@ -23,7 +19,4 @@ angular.module('starter.controllers', [])
 // })
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
 });
