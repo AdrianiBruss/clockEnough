e8bf55e1ffcd272f80d4fdb950bb9f4e1a68c0e2
AdrianiBruss <abrussolo@gmail.com>
2016-05-01 19:12:29 +0200

    reconnaissance user group

--- www/js/controllers/EventController.js
+++ www/js/controllers/EventController.js
@@ -1,5 +1,6 @@
 angular.module('clockEnough')
 
+// tab.event ( Tous les événements );
 .controller('EventCtrl', function($scope, $rootScope, ionicMaterialInk, ionicMaterialMotion, $state, FaceAPI) {
 	$scope.goTo = function ( path ) {
 		$state.go(path);
@@ -17,6 +18,7 @@ angular.module('clockEnough')
     });
 })
 
+// tab.event-check ( Detail d'un événement );
 .controller('EventCheckCtrl', function($scope, $state, $stateParams, FaceAPI, $rootScope) {
 
 	FaceAPI.getEventInfos($stateParams.eventId);
@@ -25,25 +27,12 @@ angular.module('clockEnough')
 	});
 })
 
-.controller('EventCaptureCtrl', function($scope, $state, $stateParams) {
-	console.log('capture');
-	console.log($stateParams);
-	$scope.group_id = $stateParams.eventId;
-	$scope.page_param = $stateParams.check;
-})
-
-.controller('EventCheckStatusCtrl', function($scope, $state, $stateParams) {
-	console.log('checkstatus');
-	console.log($stateParams);
-	// $scope.group_id = $stateParams.eventId;
-	// $scope.page_param = $stateParams.check;
-})
-
-.controller('EventCaptureCtrl', ['$scope', '$cordovaCamera', 'FaceAPI', '$rootScope', '$ionicPopup', '$state','$stateParams', function($scope, $cordovaCamera,FaceAPI,$rootScope,$ionicPopup,$state, $stateParams){
+// tab.event-capture ( prise en photo de l'utilisateur )
+.controller('EventCaptureCtrl', ['$scope', '$cordovaCamera', 'FaceAPI', '$rootScope', '$ionicPopup', '$state','$stateParams', '$ionicLoading', function($scope, $cordovaCamera,FaceAPI,$rootScope,$ionicPopup,$state, $stateParams, $ionicLoading){
 
     $scope.icon = true;
-
     $scope.group_id = $stateParams.eventId;
+	$scope.page_param = $stateParams.check;
 
     $scope.capturePicture = function() {
 
@@ -80,7 +69,10 @@ angular.module('clockEnough')
 
         if(angular.isDefined($scope.fileURI))
         {
-            options.fileKey = "uploaded_file";
+
+			$ionicLoading.show();
+
+			options.fileKey = "uploaded_file";
             options.fileName = String($scope.fileURI).substr(String($scope.fileURI).lastIndexOf('/') + 1);
             options.mimeType = "image/jpg";
 
@@ -106,20 +98,47 @@ angular.module('clockEnough')
 
     // reconnaissance faciale
     $scope.$on('trainEvent', function(event,data){
-        FaceAPI.recognizeUser($scope.group_id,$scope.imgUrl);
+        FaceAPI.recognizeUser($scope.group_id, $scope.imgUrl);
     });
 
     //popup d'alerte résultat
     $scope.$on('recognizeUser', function(event,data){
-       if(angular.isDefined(data.face[0])){
-            var candidate_name = data.face[0].candidate[0].person_name
-            $scope.alertUser('Vérification de l\'utilisateur', 'Bonjour ' + candidate_name + '!');
+		$scope.candidate = data.face[0].candidate[0];
+
+       if(angular.isDefined($scope.candidate) ){
+		   if ( $scope.candidate.confidence > 35 ) {
+    		   FaceAPI.getUserInfos($scope.candidate.person_id);
+		   }else {
+			   $ionicLoading.hide();
+			   $scope.alertUser('Vérification de l\'utilisateur', 'Désolé, vous ne faites pas partie de l\' événement ! Au revoir');
+		   }
         }
         else{
+			$ionicLoading.hide();
             $scope.alertUser('Vérification de l\'utilisateur', 'Nous ne parvenons pas à vous reconnaître !');
         }
     });
 
+	// FaceAPI.getUserInfos('6a6eb09dc05c64a29b668293efac74f1');
+	// verification de l'appartenance de l'utilisateur au groupe en question
+	$scope.$on('userInfos', function(event, data){
+		$ionicLoading.hide();
+		$scope.belongsTo = data.group.filter(function(group){
+			return group.group_id == $scope.group_id;
+		})
+		// l'utilisateur appartient bien au groupe
+		if ( $scope.belongsTo != [] ){
+
+			$state.go('tab.event-check-status', {
+					'eventId': $scope.group_id,
+					'param': $scope.page_param,
+					'personId' : $scope.candidate.person_id
+				}
+			)
+		} else {
+			$scope.alertUser('Vérification de l\'utilisateur', 'Bonjour ' + $scope.candidate.person_name + ', vous ne faites pas partie de la soirée ! Au revoir');
+		}
+	})
     // notifications d'alerte
     $scope.alertUser = function(title,message){
         $ionicPopup.alert({
@@ -129,3 +148,10 @@ angular.module('clockEnough')
     };
 
 }])
+
+// tab.event-check-status ( possibilité d'attribuer ou verifier un status )
+.controller('EventCheckStatusCtrl', function(FaceAPI, $scope, $state, $stateParams) {
+	console.log('checkstatus');
+	console.log($stateParams);
+
+})
